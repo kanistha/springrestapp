97c03da6f530621e2a08c761c27cae3ba99bf58b
kanistha <kanistha.a@gmail.com>
2016-12-11 17:00:41 +0530

    Initial Commit

---
+++ src/main/java/com/product/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.product;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ProductRepository {
+    public List<Product> getProducts() {
+
+        Product product = new Product();
+            return Arrays.asList(product);
+    }
+}
